bf36d6642af2d7b8b2112a8b775bd03f6351ea32
Kerydic <1515240767@qq.com>
2024-01-30 17:18:18 +0800

    [CI] Build iOS Test & Downgrade unity version to 2021.3.34f1 for switching between laptop and work pc

--- Assets/Editor/CI/CIUtil.cs
+++ Assets/Editor/CI/CIUtil.cs
@@ -11,16 +11,50 @@ namespace KDGame.Editor.CI
 {
 	public class CIUtil
 	{
-		[MenuItem(EditorMenuConst.CICD + "Build With Param", false, 2)]
-		public static void BuildWithParam()
+		[MenuItem(EditorMenuConst.CICD + "Build iOS Test", false, 2)]
+		public static void BuildIOSTest()
+		{
+			BuildWithParam(new CIParam{
+				Target = BuildTarget.iOS,
+				TargetGroup = BuildTargetGroup.iOS,
+				PkgID = "com.kerydic.kdexp.ios",
+				Version = "0.0.1",
+				BuildNum = 1,
+				CompareBuildNum = 1,
+				OutputPath = "/Users/kerydic/Downloads/kdexp_xcode_output",
+				IsDev = false,
+				IsAab = false,
+				CreateSymbol = false
+			});
+		}
+
+		public static void BuildWithParam(CIParam param)
 		{
 			Application.logMessageReceivedThreaded += CaptureLogThread;
-			var buildTarget = EditorUserBuildSettings.activeBuildTarget;
+			if (EditorUserBuildSettings.activeBuildTarget != param.Target){
+				EditorUserBuildSettings.SwitchActiveBuildTarget(param.TargetGroup, param.Target);
+			}
+			// 版本号等配置
+			PlayerSettings.SetApplicationIdentifier(param.TargetGroup, param.PkgID);
+			PlayerSettings.bundleVersion = param.Version;
+			PlayerSettings.Android.bundleVersionCode = param.BuildNum;
+			PlayerSettings.iOS.buildNumber = param.BuildNum.ToString();
+			// Android 特定配置
+			PlayerSettings.Android.useCustomKeystore = false; //true;
+			// PlayerSettings.Android.keystoreName = setting.keystorePath;
+			// PlayerSettings.Android.keystorePass = setting.keystorePass;
+			// PlayerSettings.Android.keyaliasName = setting.keyAliasName;
+			// PlayerSettings.Android.keyaliasPass = setting.keyAliasPass;
+			// iOS特定配置
+			PlayerSettings.iOS.scriptCallOptimization = ScriptCallOptimizationLevel.SlowAndSafe;
+			PlayerSettings.stripEngineCode = false;
+			AssetDatabase.SaveAssets();
 			CIHybridCLRUtil.OnPreBuild();
-			CIABUtil.BuildAllAssetBundles(buildTarget);
+			CIABUtil.BuildAllAssetBundles(param.Target);
 			// TODO 异常依赖检测
-			Application.logMessageReceivedThreaded -= CaptureLogThread;
 			// 构建
+			BuildPlayer(param);
+			Application.logMessageReceivedThreaded -= CaptureLogThread;
 		}
 
 		private static void CaptureLogThread(string condition, string stacktrace, LogType type)
@@ -37,23 +71,23 @@ namespace KDGame.Editor.CI
 			}
 		}
 
-		private static void BuildPlayer(BuildParam param)
+		private static void BuildPlayer(CIParam param)
 		{
 			var sceneNameList = new List<string>();
 			foreach (var scene in EditorBuildSettings.scenes)
 				if (scene != null && scene.enabled)
 					sceneNameList.Add(scene.path);
 			var sceneNameAry = sceneNameList.ToArray();
-			var outputDir = Path.GetDirectoryName(param.outputPath);
+			var outputDir = Path.GetDirectoryName(param.OutputPath);
 			if (!Directory.Exists(outputDir))
 				Directory.CreateDirectory(outputDir);
 			BuildOptions options = BuildOptions.None;
-			EditorUserBuildSettings.development = param.isDev;
-			EditorUserBuildSettings.allowDebugging = param.isDev;
-			EditorUserBuildSettings.connectProfiler = param.isDev;
-			EditorUserBuildSettings.buildAppBundle = param.isAppBundle;
-			EditorUserBuildSettings.androidCreateSymbolsZip = param.createSymbol;
-			if(param.isDev)
+			EditorUserBuildSettings.development = param.IsDev;
+			EditorUserBuildSettings.allowDebugging = param.IsDev;
+			EditorUserBuildSettings.connectProfiler = param.IsDev;
+			EditorUserBuildSettings.buildAppBundle = param.IsAab;
+			EditorUserBuildSettings.androidCreateSymbols = param.CreateSymbol ? AndroidCreateSymbols.Public : AndroidCreateSymbols.Disabled;
+			if(param.IsDev)
 			{
 				options |= BuildOptions.Development;
 				options |= BuildOptions.ConnectWithProfiler;
@@ -68,12 +102,12 @@ namespace KDGame.Editor.CI
 
 			try
 			{
-				Debug.Log("OutputPath:" + param.outputPath);
-				if (param.Target == BuildTarget.Android && param.isAppBundle)
+				Debug.Log("OutputPath:" + param.OutputPath);
+				if (param.Target == BuildTarget.Android && param.IsAab)
 					// 打AAB
 					BuildAAB(param, options);
 				else
-					BuildPipeline.BuildPlayer(sceneNameAry, param.outputPath, param.Target, options);
+					BuildPipeline.BuildPlayer(sceneNameAry, param.OutputPath, param.Target, options);
 				Debug.Log("Build Player Success");
 			}
 			catch (Exception exception)
@@ -82,32 +116,35 @@ namespace KDGame.Editor.CI
 			}
 			finally
 			{
-				
+				Debug.Log("Build Player Exit.");
 			}
 			// TODO 上传/缓存AssetBundle
 		}
 
-		private static void BuildAAB(BuildParam param, BuildOptions options)
+		private static void BuildAAB(CIParam param, BuildOptions options)
 		{
-			var buildPlayerOptions = AndroidBuildHelper.CreateBuildPlayerOptions(param.outputPath);
+			#if UNITY_ANDROID
+			var buildPlayerOptions = AndroidBuildHelper.CreateBuildPlayerOptions(param.OutputPath);
 			buildPlayerOptions.options = options;
 			var assetPackConfig = new AssetPackConfig();
 			assetPackConfig.SplitBaseModuleAssets = true;
 			Bundletool.BuildBundle(buildPlayerOptions, assetPackConfig);
+			#endif
 		}
 	}
 
-	public class BuildParam
+	public class CIParam
 	{
 		public BuildTarget Target;
+		public BuildTargetGroup TargetGroup;
+		public string PkgID;
 
-		public string versionStr;
-		public int buildNum;
+		public string Version;
+		public int BuildNum;
 		public int CompareBuildNum;
-		
-		public string outputPath;
-		public bool isDev;
-		public bool isAppBundle;
-		public bool createSymbol;
+		public string OutputPath;
+		public bool IsDev;
+		public bool IsAab;
+		public bool CreateSymbol;
 	}
 }
